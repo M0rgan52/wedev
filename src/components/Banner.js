480e2fdae445c34a0e3ba02eb4f6de5505a8febc
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-06 11:13:54 +0100

    commit -m "Style et animation Banner v1"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,9 +3,9 @@ import logo from "../assets/WeDev_v1.png";
 
 function Banner() {
   return (
-    <header className="wedev_banner">
-      <img src={logo} alt="Logo WeDev" />
-      <ul>
+    <header id="home" className="wedev_banner">
+      <a href="#home"><img src={logo} alt="Logo WeDev" className="banner_img"/></a>
+      <ul className="banner_list">
         <li><a href={undefined}>A propos</a></li>
         <li><a href={undefined}>Portofolio</a></li>
         <li><a href={undefined}>Skills</a></li>
