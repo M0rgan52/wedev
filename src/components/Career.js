ec65994fd8895c689bd4ce00b22b4f1a3098ff06
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-09 16:36:10 +0100

    commit - "Travail préparatoire style career"

--- src/components/Career.js
+++ src/components/Career.js
@@ -2,16 +2,16 @@ import "../styles/Career.css"
 
 function Career() {
     return (
-        <div className="Career">
-            <article>
+        <div className="career">
+            <article className="career-1">
                 <h2>Développeur Web Freelance</h2>
                 <p>Je créé vos projets avec Node.js, react.js et MongoDB</p>
             </article>
-            <article>
+            <article className="career-2">
                 <h2>Formation Développeur Web</h2>
                 <p>Diplomé avec OpenClassroom</p>
             </article>
-            <article>
+            <article className="career-3">
                 <h2>10 ans de carrière en comptabilité et fiscalité</h2>
                 <p>Dont 6 ans en cabinet d'expertise comptable</p>
             </article>
