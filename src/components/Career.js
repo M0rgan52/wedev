ce64fe36d0e0bcffd5e61cffbeaecf7737f0ea1e
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-10 11:28:15 +0100

    commit -m "Modification texte partie skill v2"

--- src/components/Career.js
+++ src/components/Career.js
@@ -5,15 +5,30 @@ function Career() {
         <div className="career">
             <article className="career-1">
                 <h2>Développeur Web Freelance</h2>
-                <p>Je créé vos projets avec Node.js, react.js et MongoDB</p>
+                <p>Je créé vos projets from scratch ou je reprends vos projets actuels</p>
             </article>
             <article className="career-2">
-                <h2>Formation Développeur Web</h2>
-                <p>Diplomé avec OpenClassroom</p>
+                <h2>Frontend Développeur</h2>
+                <p>Je développe le visuel de votre site internet ou de vos applications</p>
+                <h3>Langages :</h3>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>SASS</li>
+                    <li>JavaScript</li>
+                    <li>React</li>
+                </ul>
             </article>
             <article className="career-3">
-                <h2>10 ans de carrière en comptabilité et fiscalité</h2>
-                <p>Dont 6 ans en cabinet d'expertise comptable</p>
+                <h2>Backend Développeur</h2>
+                <p>Je développe votre base de donnée et l'ensemble des fetch nécessaire au bon fonctionnement du votre projet</p>
+                <h3>Langages :</h3>
+                <ul>
+                    <li>JavaScript</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>MongoDB</li>
+                </ul>
             </article>
 
         </div>
