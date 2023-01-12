72b35221c8dea13338b6bd3a98d849f26e2c059c
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-12 16:28:42 +0100

    commit -m "Retravail sur le DOM et travail préparatoire sur le style"

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -3,32 +3,56 @@ import ImgOMFood from "../assets/omfood_portofolio.PNG";
 
 function Portofolio() {
   return (
-    <div>
-      <div className="box_portofolio">
-        <a href={undefined}>
-          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
-        </a>
-      </div>
-      <div className="box_portofolio">
-        <a href={undefined}>
-          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
-        </a>
-      </div>
-      <div className="box_portofolio">
-        <a href={undefined}>
-          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
-        </a>
-      </div>
-      <div className="box_portofolio">
-        <a href={undefined}>
-          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
-        </a>
-      </div>
-      <div className="box_portofolio">
-        <a href={undefined}>
-          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
-        </a>
-      </div>
+    <div className="box_parent_portofolio">
+      <a href={undefined} className="link_portofolio">
+        <figure>
+          <img
+            src={ImgOMFood}
+            alt="Illustration du site Oh My Food"
+            className="img_portofolio"
+          />
+        </figure>
+      </a>
+
+      <a href={undefined} className="link_portofolio">
+        <figure>
+          <img
+            src={ImgOMFood}
+            alt="Illustration du site Oh My Food"
+            className="img_portofolio"
+          />
+        </figure>
+      </a>
+
+      <a href={undefined} className="link_portofolio">
+        <figure>
+          <img
+            src={ImgOMFood}
+            alt="Illustration du site Oh My Food"
+            className="img_portofolio"
+          />
+        </figure>
+      </a>
+
+      <a href={undefined} className="link_portofolio">
+        <figure>
+          <img
+            src={ImgOMFood}
+            alt="Illustration du site Oh My Food"
+            className="img_portofolio"
+          />
+        </figure>
+      </a>
+
+      <a href={undefined} className="link_portofolio">
+        <figure>
+          <img
+            src={ImgOMFood}
+            alt="Illustration du site Oh My Food"
+            className="img_portofolio"
+          />
+        </figure>
+      </a>
     </div>
   );
 }
