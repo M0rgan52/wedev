80090353fe297c105441c12715e1765cca3e87ff
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-12 15:52:21 +0100

    commit -m "Travail DOM portofolio"

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -1,7 +1,36 @@
-import "../styles/Portofolio.css"
+import "../styles/Portofolio.css";
+import ImgOMFood from "../assets/omfood_portofolio.PNG";
 
 function Portofolio() {
-
+  return (
+    <div>
+      <div className="box_portofolio">
+        <a href={undefined}>
+          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
+        </a>
+      </div>
+      <div className="box_portofolio">
+        <a href={undefined}>
+          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
+        </a>
+      </div>
+      <div className="box_portofolio">
+        <a href={undefined}>
+          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
+        </a>
+      </div>
+      <div className="box_portofolio">
+        <a href={undefined}>
+          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
+        </a>
+      </div>
+      <div className="box_portofolio">
+        <a href={undefined}>
+          <img src={ImgOMFood} alt="Illustration du site Oh My Food" className="img_portofolio" />
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Portofolio
+export default Portofolio;
