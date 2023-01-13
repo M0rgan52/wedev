84ff12c86e5ceb698a4c9c51d80d5404179bedd6
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-13 18:38:56 +0100

    commit -m "Ajout package fontawesome et création du DOM partie contact"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,29 @@
-import "../styles/Contact.css"
+import "../styles/Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
-    return (
-        <div>
-
-        
-            
-        </div>
-    )
+  return (
+    <div>
+      <h4>On discute ensemble ?</h4>
+      <a
+        href={"https://www.linkedin.com/in/morgan-strohl/"}
+        rel="noreferrer"
+        target="_blank">
+        <FontAwesomeIcon icon={faLinkedin} />
+      </a>
+      <a href={"https://github.com/M0rgan52"} rel="noreferrer" target="_blank">
+        <FontAwesomeIcon icon={faGithub} />
+      </a>
+      <a
+        href={"mailto:contact.wedev@gmail.com"}
+        rel="noreferrer"
+        target="_blank">
+        <FontAwesomeIcon icon={faEnvelope} />
+      </a>
+    </div>
+  );
 }
 
 export default Contact;
