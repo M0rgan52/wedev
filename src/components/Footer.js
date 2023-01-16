29877fe817f7d4f7de2479d32ad6f268ec09ba86
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-16 15:17:38 +0100

    commit -m "Finalisation DOM footer"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
-import "../styles/Footer.css"
+import "../styles/Footer.css";
+import Logo from "../assets/WeDev_v1.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCopyright } from "@fortawesome/free-regular-svg-icons";
 
 function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div className="footer">
+      <div className="footer_name">
+        <FontAwesomeIcon icon={faCopyright}></FontAwesomeIcon>
+        <p> - Morgan Strohl</p>
+      </div>
+
+      <a href="#home">
+        <img src={Logo} alt="Logo WeDev" className="footer_img" />
+      </a>
+    </div>
+  );
 }
 
 export default Footer;
