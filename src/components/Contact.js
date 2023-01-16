f166d680e8ccdaaaaa32ac1ff5be437fcb87011c
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-16 14:43:03 +0100

    commit -m "Style v1 partie contact"

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,27 +5,27 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 function Contact() {
   return (
-    <div>
+    <div className="contact">
       <h4>On discute ensemble ?</h4>
       <a
         href={"https://www.linkedin.com/in/morgan-strohl/"}
         rel="noreferrer"
         target="_blank"
-        className="icon_contact">
+        className="icon_contact icon_linkedin">
         <FontAwesomeIcon icon={faLinkedin} />
       </a>
       <a
         href={"https://github.com/M0rgan52"}
         rel="noreferrer"
         target="_blank"
-        className="icon_contact">
+        className="icon_contact icon_github">
         <FontAwesomeIcon icon={faGithub} />
       </a>
       <a
         href={"mailto:contact.wedev@gmail.com"}
         rel="noreferrer"
         target="_blank"
-        className="icon_contact">
+        className="icon_contact icon_mail">
         <FontAwesomeIcon icon={faEnvelope} />
       </a>
     </div>
