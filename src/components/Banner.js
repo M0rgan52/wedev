695ef4291e6bd16c53821abfdd077baec42950e2
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-17 11:59:13 +0100

    commit -m "Modif animation list banner"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,9 +6,9 @@ function Banner() {
     <header id="home" className="wedev_banner">
       <a href="#home"><img src={logo} alt="Logo WeDev" className="banner_img"/></a>
       <ul className="banner_list">
-        <li><a href="#about">A propos</a></li>
-        <li><a href="#portofolio">Portofolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li className="banner_about"><a href="#about">A propos</a></li>
+        <li  className="banner_portofolio"><a href="#portofolio">Portofolio</a></li>
+        <li  className="banner_contact"><a href="#contact">Contact</a></li>
       </ul>
     </header>
   );
