b840f1fb3166825889d1947f5778be090a4d5562
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-19 17:23:05 +0100

    commit -m "prettier code"

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,21 @@ import logo from "../assets/WeDev_v1.png";
 function Banner() {
   return (
     <header id="home" className="wedev_banner">
-      <a href="#home"><img src={logo} alt="Logo WeDev" className="banner_img"/></a>
+      <a href="#home">
+        <img src={logo} alt="Logo WeDev" className="banner_img" />
+      </a>
       <ul className="banner_list">
-        <li className="banner_about"><a href="#about">A propos</a></li>
-        <li  className="banner_portofolio"><a href="#portofolio">Portofolio</a></li>
-        <li  className="banner_contact"><a href="#contact">Contact</a></li>
+        <li className="banner_about">
+          <a href="#about">A propos</a>
+        </li>
+        <li className="banner_portofolio">
+          <a href="#portofolio">Portofolio</a>
+        </li>
+        <li className="banner_contact">
+          <a href="#contact">Contact</a>
+        </li>
         <label className="banner_switch">
-          <input type="checkbox" name="contrast" className="switch_box"/>
+          <input type="checkbox" name="contrast" className="switch_box" />
           <span className="switch_btn"></span>
         </label>
       </ul>
