a7136f2926d3cccfccf15b2c1aa2c90549d0ebdd
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-20 11:57:56 +0100

    commit -m "INFO CARREER - Changement description bloc Développeur Web Freelance"

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,38 +1,50 @@
-import "../styles/Career.css"
+import "../styles/Career.css";
 
 function Career() {
-    return (
-        <div className="career" id="about">
-            <article className="career-1">
-                <h2>Développeur Web Freelance</h2>
-                <p>Je créé vos projets from scratch ou je reprends vos projets actuels</p>
-            </article>
-            <article className="career-2">
-                <h2>Frontend Développeur</h2>
-                <p>Je développe le visuel de votre site internet ou de vos applications</p>
-                <h3>Langages :</h3>
-                <ul>
-                    <li>HTML</li>
-                    <li>CSS</li>
-                    <li>SASS</li>
-                    <li>JavaScript</li>
-                    <li>React</li>
-                </ul>
-            </article>
-            <article className="career-3">
-                <h2>Backend Développeur</h2>
-                <p>Je développe votre base de donnée et l'ensemble des fetch nécessaire au bon fonctionnement du votre projet</p>
-                <h3>Langages :</h3>
-                <ul>
-                    <li>JavaScript</li>
-                    <li>Node</li>
-                    <li>Express</li>
-                    <li>MongoDB</li>
-                </ul>
-            </article>
-
-        </div>
-    )
-};
+  return (
+    <div className="career" id="about">
+      <article className="career-1">
+        <h2>Développeur Web Freelance</h2>
+        <p>
+          Mon savoir faire englobe la création, la maintenance et la mise à jour
+          de site internet, de vos logiciel et de la vie digitale de votre
+          entreprise plus généralement. <br />
+          <br />
+          Travailleur indépendant, réactif et organisé, je serai heureux de vous
+          accompagner dans vos différents projets professionnels ou personnels
+          sur du front-end ou du back-end.
+        </p>
+      </article>
+      <article className="career-2">
+        <h2>Frontend Développeur</h2>
+        <p>
+          Je développe le visuel de votre site internet ou de vos applications
+        </p>
+        <h3>Langages :</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>SASS</li>
+          <li>JavaScript</li>
+          <li>React</li>
+        </ul>
+      </article>
+      <article className="career-3">
+        <h2>Backend Développeur</h2>
+        <p>
+          Je développe votre base de donnée et l'ensemble des fetch nécessaire
+          au bon fonctionnement du votre projet
+        </p>
+        <h3>Langages :</h3>
+        <ul>
+          <li>JavaScript</li>
+          <li>Node</li>
+          <li>Express</li>
+          <li>MongoDB</li>
+        </ul>
+      </article>
+    </div>
+  );
+}
 
 export default Career;
