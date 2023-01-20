f892f8faf841984b89be383e09d88b81f6a5fa42
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-20 16:10:17 +0100

    commit -m "CAREER - Animation des blocs au scroll + instal gsap"

--- src/components/Career.js
+++ src/components/Career.js
@@ -1,9 +1,94 @@
 import "../styles/Career.css";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
 function Career() {
+  const slideInTop = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        y: 150,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: delay || 0.4,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "top center",
+        },
+      }
+    );
+  };
+
+  const slideInLeft = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        x: -150,
+        y: -50,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        y: 0,
+        delay: delay || 0.4,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "top center",
+        },
+      }
+    );
+  };
+
+  const slideInRight = (elem, delay, duration) => {
+    gsap.fromTo(
+      elem,
+      {
+        opacity: 0,
+        x: 150,
+        y: 50,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        y: 0,
+        delay: delay || 0.4,
+        duration: duration || 0.6,
+        scrollTrigger: {
+          trigger: elem,
+          start: "top center",
+          end: "bottom center",
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    slideInLeft("#career-1");
+  }, []);
+
+  useEffect(() => {
+    slideInTop("#career-2", "0.6", "0.7");
+  }, []);
+
+  useEffect(() => {
+    slideInRight("#career-3", "0.8", "0.8");
+  }, []);
+
   return (
     <div className="career" id="about">
-      <article className="career-1">
+      <article id="career-1" className="career-1">
         <h2>Développeur Web Freelance</h2>
         <p>
           Mon savoir faire englobe la création, la maintenance et la mise à jour
@@ -15,7 +100,7 @@ function Career() {
           sur du front-end ou du back-end.
         </p>
       </article>
-      <article className="career-2">
+      <article id="career-2" className="career-2">
         <h2>Frontend Développeur</h2>
         <p>
           Je développe le visuel de votre site internet ou de vos applications
@@ -29,7 +114,7 @@ function Career() {
           <li>React</li>
         </ul>
       </article>
-      <article className="career-3">
+      <article id="career-3" className="career-3">
         <h2>Backend Développeur</h2>
         <p>
           Je développe votre base de donnée et l'ensemble des fetch nécessaire
