ebe52052b0e7283f602ecce2bd6d61c96413ee26
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-20 12:18:33 +0100

    commit -m "ABOUT - Modification DOM et éléments"

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,8 @@
-import ImgAbout from "../assets/Photo_about_v1.jpg";
 import "../styles/About.css";
 
 function About() {
   return (
     <div className="about">
-      <img
-        src={ImgAbout}
-        alt="Elément de présentation d'un homme en train de coder sur un ordinateur portable. Crédit Lukas"
-        className="about_img"
-      />
     </div>
   );
 }
