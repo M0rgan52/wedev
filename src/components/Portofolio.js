31ef5442c2d44d5c781a49a7d2ccb3506427c684
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-25 12:26:12 +0100

    commit -m "PORTOFOLIO - Mise à jour de tout les textes de toutes les box"

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -1,6 +1,6 @@
 import "../styles/Portofolio.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHtml5, faCss3, faJs } from "@fortawesome/free-brands-svg-icons";
+import { faHtml5, faCss3, faJs, faReact } from "@fortawesome/free-brands-svg-icons";
 import ImgOMFood from "../assets/omfood_portofolio.PNG";
 import ImgLapanthere from "../assets/lapanthere_portofolio.png";
 
@@ -46,9 +46,16 @@ function Portofolio() {
 
         <a href={undefined} className="link_portofolio">
           <figure>
+                      <article className="text_portofolio">
+              <h3>Kanap</h3>
+              <p>Front-end d'un site marchand de vente de canapé</p>
+              <FontAwesomeIcon icon={faHtml5} className="icon_portofolio html5"/>
+              <FontAwesomeIcon icon={faCss3} className="icon_portofolio css3" />
+              <FontAwesomeIcon icon={faJs} className="icon_portofolio js" />
+            </article>
             <img
-              src={ImgOMFood}
-              alt="Illustration du site Oh My Food"
+              src={undefined}
+              alt="Illustration du site Kanap"
               className="img_portofolio"
             />
           </figure>
@@ -56,9 +63,16 @@ function Portofolio() {
 
         <a href={undefined} className="link_portofolio">
           <figure>
+                                  <article className="text_portofolio">
+              <h3>Piiquante</h3>
+              <p>Back-end d'un site de notation de sauce piquante</p>
+              <FontAwesomeIcon icon={faHtml5} className="icon_portofolio html5"/>
+              <FontAwesomeIcon icon={faCss3} className="icon_portofolio css3" />
+              <FontAwesomeIcon icon={faJs} className="icon_portofolio js" />
+            </article>
             <img
-              src={ImgOMFood}
-              alt="Illustration du site Oh My Food"
+              src={undefined}
+              alt="Illustration du site Piiquante"
               className="img_portofolio"
             />
           </figure>
@@ -66,9 +80,17 @@ function Portofolio() {
 
         <a href={undefined} className="link_portofolio">
           <figure>
+                                  <article className="text_portofolio">
+              <h3>Groupomania</h3>
+              <p>Réseau social de l'entreprise Groupomania</p>
+              <FontAwesomeIcon icon={faHtml5} className="icon_portofolio html5"/>
+              <FontAwesomeIcon icon={faCss3} className="icon_portofolio css3" />
+              <FontAwesomeIcon icon={faJs} className="icon_portofolio js" />
+              <FontAwesomeIcon icon={faReact} className="icon_portofolio react" />
+            </article>
             <img
-              src={ImgOMFood}
-              alt="Illustration du site Oh My Food"
+              src={undefined}
+              alt="Illustration du réseau social interne Groupomania"
               className="img_portofolio"
             />
           </figure>
