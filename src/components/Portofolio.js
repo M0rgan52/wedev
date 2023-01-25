d48b1aa060748481b22cd13e7961798789e10b58
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-25 11:37:14 +0100

    commit -m "PORTOFOLIO - couleur icone langage"

--- src/components/Portofolio.js
+++ src/components/Portofolio.js
@@ -1,7 +1,6 @@
 import "../styles/Portofolio.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHtml5, faCss3 } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import ImgOMFood from "../assets/omfood_portofolio.PNG";
 
 function Portofolio() {
@@ -16,8 +15,8 @@ function Portofolio() {
             <article className="text_portofolio">
               <h3>Oh my food</h3>
               <p>Site de réservation dans des restaurants gastronomique</p>
-              <FontAwesomeIcon icon={faHtml5} className="icon_portofolio"/>
-              <FontAwesomeIcon icon={faCss3} className="icon_portofolio" />
+              <FontAwesomeIcon icon={faHtml5} className="icon_portofolio html5"/>
+              <FontAwesomeIcon icon={faCss3} className="icon_portofolio css3" />
             </article>
             <img
               src={ImgOMFood}
