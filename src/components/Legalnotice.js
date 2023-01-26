29a5e348d882005a16eeecba6fee8ee2eee9bc54
Morgan Strohl <morgan.strohl@gmail.com>
2023-01-26 14:42:43 +0100

    commit -m "LEGAL - Texte mentions légale et mise en page"

--- src/components/Legalnotice.js
+++ src/components/Legalnotice.js
@@ -1,10 +1,37 @@
+import "../styles/Legalnotice.css";
 
 function PageLegalNotice() {
-    return (
-        <div>
-           
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="titre_legal">Mentions legales</h1>
+      <h2>Identité</h2>
+      <p className="texte_legal">
+        Morgan Strohl
+        <br />
+        Nom commercial: WeDev
+        <br />
+        76 rue de la clef des chammps
+        <br />
+        95180 MENUCOURT
+        <br />
+        FRANCE
+        <br />
+        contact: contact.wedev@gmail.com
+        <br />
+        SIRET: 947 872 644 00010
+      </p>
+      <h2>Propriété intellectuelle</h2>
+      <p className="texte_legal">
+        Photo d'illustration de la page d'accueil: libre de droit, photo prise
+        par Lukas et mis à disposition sur le site pexels
+      </p>
+      <h2>Hébergement du site</h2>
+      <p className="texte_legal">
+        Amet minim sunt proident cupidatat cupidatat aliqua et ullamco quis
+        aliquip.
+      </p>
+    </div>
+  );
 }
 
 export default PageLegalNotice;
